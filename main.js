2b96ecccc17e9c5a9c42e66afa4f4a20f94e8c1c
YunmiSeo <YunmiSeo@users.noreply.github.com>
2020-07-13 12:44:51 +0200

    update time stamp

--- main.js
+++ main.js
@@ -19,6 +19,7 @@ function stopVideo() {
   video.currentTime = 0;
 }
 
+// change play and puase icon
 function changeIcon() {
   if (video.paused) {
     play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
@@ -26,11 +27,32 @@ function changeIcon() {
     play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
   }
 }
+
+// update progress and timestamp
+function updateProgress() {
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  //get minutes
+  let minutes = Math.floor(video.currentTime / 60);
+  if (minutes < 10) {
+    minutes = "0" + String(minutes);
+  }
+
+  //get seconds
+  let seconds = Math.floor(video.currentTime % 60);
+  if (seconds < 10) {
+    seconds = "0" + String(seconds);
+  }
+
+  timestamp.innerHTML = `${minutes}:${seconds}`;
+}
+
 // EventListeners
 
 video.addEventListener("click", toggleVideoStatus);
 video.addEventListener("play", changeIcon);
 video.addEventListener("pause", changeIcon);
+video.addEventListener("timeupdate", updateProgress);
 
 play.addEventListener("click", toggleVideoStatus);
 stop.addEventListener("click", stopVideo);
